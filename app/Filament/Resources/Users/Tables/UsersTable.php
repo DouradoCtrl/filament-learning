3b515182bd66add70d39b812c946169b2b860d73
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-18 01:21:20 -0300

    add: coluna is_admin usando iconColumn

---
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources\Users\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Table;
+
+class UsersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nome')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('E-mail')
+                    ->searchable(),
+
+                IconColumn::make('is_admin')
+                    ->label('Admin?')
+                    ->icon(function ($state) {
+                        return $state ? 'heroicon-o-check-circle' : 'heroicon-o-x-circle';
+                    })
+                    ->color(function ($state) {
+                        return $state ? 'success' : 'danger';
+                    }),
+                
+                TextColumn::make('email_verified_at')
+                    ->label('Email verificado em')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('created_at')
+                    ->label('Criado em')
+                    ->sortable()
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->label('Atualizado em')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('comments_count')
+                    ->label('Comentários')
+                    ->counts('comments')
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                // ViewAction::make(),
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
