a1afb09bc5ce4d1b30c5d56158de062b2f8a19e0
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-18 02:37:00 -0300

    add: resource interface backup

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,11 +28,11 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->font('Nova Square')
+            // ->font('Nova Square')
             ->brandName('Dourado Dev')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Indigo,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
