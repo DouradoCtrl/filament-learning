0ba0b8a085c09a7f054b555316d064d2c689cbf6
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-18 01:27:38 -0300

    edit: alterando ícone /p OutilinedUsers

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -20,9 +20,9 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::Users;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUsers;
 
-    protected static ?string $label = 'Usuários';
+    protected static ?string $modelLabel = 'Usuários';
 
     protected static ?string $recordTitleAttribute = 'name';
 
