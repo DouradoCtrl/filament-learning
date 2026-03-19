dd6c7c1f137f664fdae2195cce401bab40e9a9a3
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-19 16:58:02 -0300

    add: adiconado campo is_published em fadtory de post

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,6 +17,7 @@ class PostFactory extends Factory
         return [
             'title' => fake()->sentence(4),
             'slug' => fake()->slug(),
+            'is_published' => rand(0, 1),
             'content' => fake()->paragraphs(3, true),
             'user_id' => User::factory(),
             'tag_id' => Tag::factory(),
