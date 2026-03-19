59ab9d55dcdab9f75f16bd4683ead21ed1d3f1a6
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-19 13:35:03 -0300

    add: adiconado novo layout

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -6,6 +6,7 @@ use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\FileUpload;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -14,47 +15,71 @@ class UserForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->rules(['required', 'min:10'])
-                    ->label('Nome')
-                    ->placeholder('Digite o nome do usuário')
-                    ->validationMessages([
-                        'required' => 'O nome é obrigatório.',
-                        'min' => 'O campo nome deve conter no mínimo :min caracteres. Por gentileza, informe um nome completo.',
-                    ])
-                    ->required(),
+                Section::make('Informações do usuário')
+                    ->description('Dados principais do usuário.')
+                    ->columns(2)
+                    ->columnSpanFull()
+                    ->collapsible()
+                    ->schema([
+                        TextInput::make('name')
+                            ->rules(['required', 'min:10'])
+                            ->label('Nome')
+                            ->placeholder('Digite o nome do usuário')
+                            ->hint("Nome do usuário")
+                            ->validationMessages([
+                                'required' => 'O nome é obrigatório.',
+                                'min' => 'O campo nome deve conter no mínimo :min caracteres. Por gentileza, informe um nome completo.',
+                            ])
+                            ->required(),
+        
+                        TextInput::make('email')
+                            ->rules(['required'])
+                            ->unique()
+                            ->email()
+                            ->label('E-mail')
+                            ->placeholder('Digite o email do usuário')
+                            ->hint("Email do usuário")
+                            ->email()
+                            ->required(),
+        
+                        TextInput::make('password')
+                            ->rules(['required'])
+                            ->password()
+                            ->placeholder('Digite a senha do usuário')
+                            ->hint("Senha do usuário")
+                            ->required()
+                            ->visibleOn(['create']),
+        
+                        TextInput::make('phone')
+                            ->label('Telefone')
+                            ->mask('(61) 99999-9999')
+                            ->placeholder('(61) 99999-9999')
+                            ->hint("Telefone do usuário"),
+                    ]),
 
-                TextInput::make('email')
-                    ->rules(['required'])
-                    ->unique()
-                    ->email()
-                    ->label('E-mail')
-                    ->placeholder('Digite o email do usuário')
-                    ->email()
-                    ->required(),
+                Section::make('Avatar')
+                    ->description('Defina o avatar do usuário.')
+                    ->columns(2)
+                    ->schema([
+                        FileUpload::make('avatar')
+                            ->label('Anexo')
+                            ->image()
+                            ->imageEditor()
+                            ->directory('avatars')
+                            ->helperText('Faça o upload da imagem de perfil do usuário.')
+                            ->columnSpanFull(),
+                    ])->columnSpanFull(),
 
-                TextInput::make('password')
-                    ->rules(['required'])
-                    ->password()
-                    ->placeholder('Senha do usuário')
-                    ->required()
-                    ->visibleOn(['create']),
-
-                TextInput::make('phone')
-                    ->label('Telefone')
-                    ->mask('(61) 99999-9999')
-                    ->placeholder('(61) 99999-9999'),
-
-                Toggle::make('is_admin')
-                    ->label('Administrador?')
-                    ->helperText('Indica se o usuário tem privilégios de administrador.'),
-
-                FileUpload::make('avatar')
-                    ->label('Anexo')
-                    ->avatar()
-                    ->imageEditor()
-                    ->directory('avatars')
-                    ->helperText('Faça o upload da imagem de perfil do usuário.'),
-            ]);
+                Section::make('Privilégios de administrador')
+                    ->description('Configurações de privilégios de administrador.')
+                    ->columns(2)
+                    ->schema([
+                        Toggle::make('is_admin')
+                            ->label('Administrador')
+                            ->hint('Defina se o usuário tem privilégios de administrador.')
+                            ->helperText('Usuário é admin?')
+                            ->columnSpanFull(),
+                    ])->columnSpanFull()
+        ]);
     }
 }
