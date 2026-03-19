84ae5f757bff71599ba512888dc04117c4a3c658
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-19 16:56:41 -0300

    add: adicionado relacionamento de categorias com post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -57,6 +57,11 @@ class Post extends Model
         return $this->belongsTo(Tag::class);
     }
 
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
     public function tags(): HasMany
     {
         return $this->hasMany(Tag::class);
