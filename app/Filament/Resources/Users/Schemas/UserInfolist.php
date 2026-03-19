acf750e5a409febaf8c2bb01ca86bb9cfbb6a117
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-19 09:24:34 -0300

    add: criado visualização de perfil de usuário

--- app/Filament/Resources/Users/Schemas/UserInfolist.php
+++ app/Filament/Resources/Users/Schemas/UserInfolist.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\Users\Schemas;
 
 use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\ImageEntry;
+use \Filament\Infolists\Components\IconEntry;
 use Filament\Schemas\Schema;
 
 class UserInfolist
@@ -11,16 +13,28 @@ class UserInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('name'),
+                //exibir avatar
+                ImageEntry::make('avatar')
+                    ->label('Avatar')
+                    ->circular(),
+                TextEntry::make('name')
+                    ->label('Name'),
                 TextEntry::make('email')
-                    ->label('Email address'),
-                TextEntry::make('email_verified_at')
-                    ->dateTime()
-                    ->placeholder('-'),
+                    ->label('Endereço de e-mail'),
+                TextEntry::make('phone')
+                    ->label('Telefone'),
+                IconEntry::make('is_admin')
+                    ->label('Administrador?')
+                    ->boolean(),
+                TextEntry::make('comments_count')
+                    ->label('Número de comentários')
+                    ->counts('comments'),
                 TextEntry::make('created_at')
+                    ->label("Criado em")
                     ->dateTime()
                     ->placeholder('-'),
                 TextEntry::make('updated_at')
+                    ->label("Atualizado em")
                     ->dateTime()
                     ->placeholder('-'),
             ]);
