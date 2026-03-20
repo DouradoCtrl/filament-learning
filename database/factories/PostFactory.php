a6c01ae6e2d8070df7c4c8bc8d630239640101a2
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-20 14:42:21 -0300

    add: adicionado url placehold no atributo thumbnail da factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,10 +17,10 @@ class PostFactory extends Factory
         return [
             'title' => fake()->sentence(4),
             'slug' => fake()->slug(),
+            'thumbnail' => "https://placehold.co/640x480/png?text=Thumbnail",
             'is_published' => rand(0, 1),
             'content' => fake()->paragraphs(3, true),
             'user_id' => User::factory(),
-            'tag_id' => Tag::factory(),
             'category_id' => Category::factory(),
             'created_at' => fake()->dateTime(),
             'updated_at' => fake()->dateTime(),
