fcf18410be5dec0e554674cda95ae71834a3e386
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-25 16:28:22 -0300

    add: Instalado e configurado resource para fazer o export da tabela de usuários

---
+++ app/Filament/Exports/UserExporter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Exports;
+
+use App\Models\User;
+use Filament\Actions\Exports\ExportColumn;
+use Filament\Actions\Exports\Exporter;
+use Filament\Actions\Exports\Models\Export;
+use Illuminate\Support\Number;
+
+class UserExporter extends Exporter
+{
+    protected static ?string $model = User::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ExportColumn::make('name')
+                ->label('Nome'),
+            ExportColumn::make('email')
+                ->label('E-mail'),
+            // ExportColumn::make('phone')
+            //     ->label('telefone'),
+            ExportColumn::make('created_at')
+                ->label('Criado em')
+                ->formatStateUsing(function ($state) {
+                    return $state->format('d/m/y H:i:s');
+                })
+
+        ];
+    }
+
+    public static function getCompletedNotificationBody(Export $export): string
+    {
+        $body = 'Your user export has completed and ' . Number::format($export->successful_rows) . ' ' . str('row')->plural($export->successful_rows) . ' exported.';
+
+        if ($failedRowsCount = $export->getFailedRowsCount()) {
+            $body .= ' ' . Number::format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to export.';
+        }
+
+        return $body;
+    }
+}
