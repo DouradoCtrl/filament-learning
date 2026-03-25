ad90c2129b8c0c4dfd56b93dba259e3ca9896c4b
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-25 13:52:04 -0300

    add: adicionado aba de todos, administrador e usuário na exibição de tabela de usuários

--- app/Filament/Resources/Users/Pages/ListUsers.php
+++ app/Filament/Resources/Users/Pages/ListUsers.php
@@ -5,10 +5,43 @@ namespace App\Filament\Resources\Users\Pages;
 use App\Filament\Resources\Users\UserResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Filament\Support\Icons\Heroicon;
 
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
+
+    public function getTabs(): array
+    {
+        return [
+            'Todos' => Tab::make()
+                ->icon(Heroicon::OutlinedDocument)
+                ->badge(function () {
+                    return static::getModel()::count();
+                })
+                ->badgeColor('primary'),
+            'Administrador' => Tab::make()
+                ->query(function ($query) {
+                    return $query->where('is_admin', true);
+                })
+                ->icon(Heroicon::OutlinedCheckCircle)
+                ->badge(function () {
+                     return static::getModel()::where('is_admin', true)->count();
+                })
+                ->badgeColor('success')
+                ,
+            'Usuário' => Tab::make()
+                ->query(function ($query) {
+                    return $query->where('is_admin', false);
+                })
+                ->icon(Heroicon::OutlinedXCircle)
+                ->badge(function (){
+                     return static::getModel()::where('is_admin', false)->count();
+                })
+                ->badgeColor('warning')
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
