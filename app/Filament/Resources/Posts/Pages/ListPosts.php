a7f0da011d9f8383b2d979a5ff5c93248aff7bb0
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-25 13:32:48 -0300

    add: adiconando abas dinâmicas na exibição de tabelas

--- app/Filament/Resources/Posts/Pages/ListPosts.php
+++ app/Filament/Resources/Posts/Pages/ListPosts.php
@@ -5,10 +5,41 @@ namespace App\Filament\Resources\Posts\Pages;
 use App\Filament\Resources\Posts\PostResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Filament\Support\Icons\Heroicon;
+
+// use Illuminate\Database\Eloquent\Builder;
 
 class ListPosts extends ListRecords
 {
     protected static string $resource = PostResource::class;
+
+    public function getTabs(): array
+    {
+        return [
+            'Todos' => Tab::make()
+                ->icon(Heroicon::OutlinedDocument),
+            'Publicados' => Tab::make()
+                ->query(function ($query) {
+                    return $query->where('is_published', true);
+                })
+                ->icon(Heroicon::OutlinedCheckCircle)
+                ->badge(function () {
+                     return static::getModel()::where('is_published', true)->count();
+                })
+                ->badgeColor('success')
+                ,
+            'Não Publicados' => Tab::make()
+                ->query(function ($query) {
+                    return $query->where('is_published', false);
+                })
+                ->icon(Heroicon::OutlinedXCircle)
+                ->badge(function (){
+                     return static::getModel()::where('is_published', false)->count();
+                })
+                ->badgeColor('danger')
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
