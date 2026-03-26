b490bf81129e09efb047dc15eb202e843d01214b
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-25 22:38:04 -0300

    edit: removido botão p/ criação de export

--- app/Filament/Resources/Exports/Pages/ListExports.php
+++ app/Filament/Resources/Exports/Pages/ListExports.php
@@ -13,7 +13,7 @@ class ListExports extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            // CreateAction::make(),
         ];
     }
 }
