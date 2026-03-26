4cd94ed73aa09d7e6a58b2651037d1a6a95f5ab2
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-26 13:54:03 -0300

    edit: aprimorado visualização userinfolist

--- app/Filament/Resources/Users/Schemas/UserInfolist.php
+++ app/Filament/Resources/Users/Schemas/UserInfolist.php
@@ -6,6 +6,7 @@ use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\ImageEntry;
 use \Filament\Infolists\Components\IconEntry;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 
 class UserInfolist
 {
@@ -20,30 +21,30 @@ class UserInfolist
                 TextEntry::make('name')
                     ->label('Name'),
                 TextEntry::make('email')
+                    ->icon(Heroicon::OutlinedEnvelope)
                     ->label('Endereço de e-mail'),
                 TextEntry::make('email_verified_at')
                     ->label('Email verificado em')
                     ->dateTime()
                     ->placeholder('-'),
                 TextEntry::make('phone')
+                    ->icon(Heroicon::OutlinedPhone)
                     ->label('Telefone'),
                 IconEntry::make('is_admin')
                     ->label('Administrador?')
                     ->boolean(),
                 TextEntry::make('comments_count')
+                    ->icon(Heroicon::OutlinedChatBubbleOvalLeft)
                     ->label('Número de comentários')
                     ->counts('comments'),
                 TextEntry::make('posts_count')
+                    ->icon(Heroicon::OutlinedDocumentText)
                     ->label('Número de posts')
                     ->counts('posts'),   
                 TextEntry::make('created_at')
                     ->label("Criado em")
                     ->dateTime()
                     ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->label("Atualizado em")
-                    ->dateTime()
-                    ->placeholder('-'),
-            ]);
+            ])->columns(3);
     }
 }
