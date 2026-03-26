468b810d572da024d9b99c11dff43ca6e5472c18
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-25 22:38:37 -0300

    add: criado tabela e ação responsável por fazer o dowload do botão

--- app/Filament/Resources/Exports/Tables/ExportsTable.php
+++ app/Filament/Resources/Exports/Tables/ExportsTable.php
@@ -5,7 +5,11 @@ namespace App\Filament\Resources\Exports\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Filament\Actions\Action;
+use App\Models\Export;
 
 class ExportsTable
 {
@@ -13,13 +17,26 @@ class ExportsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('file_name')
+                    ->icon(Heroicon::OutlinedTableCells)
+                    ->label('Nome do arquivo'),
+                TextColumn::make('completed_at')
+                    ->label('Exportado em')
+                    ->icon(Heroicon::OutlinedClock)
+                    ->dateTime('d/m/Y H:i:s'),
+                
             ])
             ->filters([
                 //
             ])
             ->recordActions([
-                EditAction::make(),
+                Action::make('download')
+                    ->label('Baixar')
+                    ->icon(Heroicon::OutlinedArrowDownTray)
+                    ->url(function ($record) {
+                        return route('filament.exports.download', ['export' => $record, 'format' => 'xlsx'], absolute: false);
+                    }
+                )
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
