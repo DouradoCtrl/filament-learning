458c1391442da10453965bf563ba03e2d623638a
DouradoCtrl <gdouradoestudante@gmail.com>
2026-03-26 13:30:30 -0300

    add: adicionado botão de deletar e ditar n header actions

--- app/Filament/Resources/Posts/Pages/ViewPost.php
+++ app/Filament/Resources/Posts/Pages/ViewPost.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\Posts\Pages;
 
 use App\Filament\Resources\Posts\PostResource;
 use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Support\Icons\Heroicon;
 
 class ViewPost extends ViewRecord
 {
@@ -13,7 +15,17 @@ class ViewPost extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            EditAction::make(),
+            EditAction::make()
+                ->label('Editar Publicação')
+                ->icon(Heroicon::Pencil)
+                ->size('sm')
+                ->color('primary')
+                ->slideOver(),
+            DeleteAction::make()
+                ->label('Excluir Publicação')
+                ->icon(Heroicon::Trash)
+                ->size('sm')
+                ->color('danger')
         ];
     }
 }
